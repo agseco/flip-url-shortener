353e745f14fada4507880bc1660466235d68feea
Alejandro Garcia Seco <alejandro.garcia@wallapop.com>
2025-03-12 10:24:20 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "com.flip.urlshortener"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
